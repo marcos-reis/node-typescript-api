bea7fcbbc183aca0aa2f17de94205ef70c3d09a7
Your Name <marcos.reis@localhost.com>
2021-08-30 22:14:10 -0300

    add type in jest mock

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,15 +6,14 @@ import stormGlassNormalize3HoursFixture from '@test/fixtures/stormglass_normaliz
 jest.mock('axios');
 
 describe('StormGlass Client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('Should return the normalize forecast from the StormGlass service', async () => {
     const lat = -33.79377;
     const lng = 54.23425;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual(stormGlassNormalize3HoursFixture);
